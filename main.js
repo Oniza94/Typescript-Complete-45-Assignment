27105e35d44137315e39aff7450b3754e17d8285
Oniza Fazal <160499036+Oniza94@users.noreply.github.com>
2024-12-28 14:04:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var book = {
+    title: "Jannat k Pattay",
+    author: "Nimra Ahmeed",
+    yearPublished: 2017
+};
+console.log;
+{
+    "Book Info: ".concat(book.title, " by {book.author} published in the year ").concat(book.yearPublished);
+}
+;
---
+++ main.ts
@@ -0,0 +1,10 @@
+let book: {title: string; author: string; yearPublished: number} =
+
+{
+title: "Jannat k Pattay",
+author: "Nimra Ahmeed",
+yearPublished: 2017
+
+};
+
+console.log{`Book Info: ${book.title} by {book.author} published in the year ${book.yearPublished}`};
